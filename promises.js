5cd27c3f32d464b7b8e8864d7412b5cb64ca327a
somu878 <somumydhili19@gmail.com>
2023-10-01 20:19:44 +0530

    added promises.js

--- promises.js
+++ promises.js
@@ -1,8 +1,18 @@
-let p1  = Promise.resolve("Hello,World!")
+let p1  = Promise.resolve("Hello,World!");
+//promise initialzation without anything
 let p2  = 404
+//promise initialization using new
 let p3  = new Promise((resolve,reject)=>{
+    //adding timeout function
     setTimeout(resolve,1000,'Hello World Sucess!')
 }
 );
-
+let p4  = Promise.reject('rejected Promise!!');
+//Gives all 
 Promise.all([p1,p2,p3]).then((val)=> console.log(val));
+//status of promises
+Promise.allSettled([p1,p2,p3]).then((val)=> console.log(val));
+//gives which executes first
+Promise.race([p1,p2,p3]).then((val)=> console.log(val));
+//returns anything which is resolved
+Promise.any([p1,p4]).then((val)=> console.log(val));
